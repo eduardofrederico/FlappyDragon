2df8c13ec5cf2c5e71c49fd8d4a1eae5c0ffcbf9
Eduardo Frederico <eduardo.frederico@icloud.com>
2025-02-20 21:16:07 -0300

    Initial Commit

---
+++ FlappyDragonTests/FlappyDragonTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlappyDragonTests.swift
+//  FlappyDragonTests
+//
+//  Created by Eduardo Frederico on 20/02/25.
+//
+
+import Testing
+@testable import FlappyDragon
+
+struct FlappyDragonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
